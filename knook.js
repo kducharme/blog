bcbe1944b934f3c980f1d76b98a1259db5b63158
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-03-16 17:25:41 -0500

    hooked up firebase :fire:

--- knook.js
+++ knook.js
@@ -1,10 +1,63 @@
 
-// Adds post information to Firebase
-function addPostToDb() {
-    let title = document.getElementById("title").value;
-    let dbRef = firebase.database().ref().child('text');
-    dbRef.on('value', snap => title.innerHTML = snap.val());
-    console.log(title)
+document.getElementById("submit-post").addEventListener("click", createPost)
+
+
+// Post object template
+function Post(title, body, img, date, author, status) {
+    this.title = title;
+    this.body = body;
+    this.img = img;
+    this.date = date;
+    this.author = author;
+    this.status = status;
 }
 
-document.getElementById("submit-post").addEventListener("click", addPostToDb)
+function createPost() {
+    let title= document.getElementById("title")
+    let body = document.getElementById("body")
+
+    newPost = new Post(title.value, body.value)
+
+    $.ajax({
+        url: 'https://kyle-personal-blog.firebaseio.com/posts.json',
+        type: "POST",
+        data: JSON.stringify(newPost),
+        success: function () {
+            console.log("success");
+        },
+        error: function(error) {
+            console.log("error: " + error)
+        }
+    });
+    clearPost(title, body)
+    console.log(newPost)
+}
+
+function clearPost(title, body) {
+    title.value = ""
+    body.value = ""
+}
+
+function getDate() {
+    let dayNames = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+    let monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let date = new Date();
+
+    let dayOfWeek = date.getDay();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let year = date.getFullYear();
+
+    let reviewDay = dayNames[dayOfWeek]
+    let reviewMonth = monthNames[month]
+
+    datePosted = reviewDay + " " + reviewMonth + " " + day + ", " + year
+
+    return datePosted;
+}
+
+function postDate() {
+    getDate()
+    
+    console.log(datePosted)
+}
